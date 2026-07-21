3fc4f14eeef6a9a8128761fa61be3ee8a1572640
Sugeng Sulistiyawan <sugeng.sulistiyawan@gmail.com>
2026-07-21 10:28:23 +0700

    refactor: convert logo to Astro components and use horizontal logo in header

--- src/constants.ts
+++ src/constants.ts
@@ -3,5 +3,5 @@ export const X_URL = 'https://x.com/docobyte';
 export const EMAIL = '';
 export const IG_URL = 'https://www.instagram.com/docobyte';
 export const TIKTOK_URL = 'https://www.tiktok.com/@docobyte';
-export const FB_URL = '';
+export const FB_URL = 'https://www.facebook.com/profile.php?id=61591799471879';
 
